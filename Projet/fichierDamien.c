fa7b4e2233c82079411c69ea0ac82102b731cbe5
Damien Cornuejouls <damien.cornuejouls@univ-tlse3.fr>
2022-04-15 17:47:32 +0200

    Ajout de struct Salle et suppression de Edt, semaine et jour

--- Projet/fichierDamien.c
+++ Projet/fichierDamien.c
@@ -8,25 +8,32 @@ struct s_cours{
     int horaire;    // Entre 1 et 20  : 1-4 = lundi, 5-8 = mardi, ... 16-20 = vendredi
     UE ue;
     Prof prof;
+    Salle salle;
 }S_cours;
 
 struct s_prof{
     int id;
-    string nom;
-    Cours[20] cours;
+    char* nom;
+    Cours* cours;
 }S_prof;
 
 struct s_Formation{
     int id;
-    string nom;
+    char* nom;
     UE* ue;
+    Cours* cours;
 }S_Formation;
 
 struct s_UE{
     int id;
-    string nom;
-    Cours[20] cours;
+    char* nom;
+    Cours* cours;
 }S_UE;
+
+struct s_Salle{
+    int nom;
+    Cours* cours;
+}S_Salle;
 
 
 void ajouteCours();
